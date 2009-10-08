3721bef61bddbd80f46ebd03ff54364fe9913d3c
Justin Blake <justin@megablaix.com>
2009-10-08 11:10:59 -0400

    added a 2D implementation of mutation with selection

---
+++ mutation_with_selection_2d.rb
@@ -0,0 +1,74 @@
+require 'mutation_with_selection'
+
+class EvolutionMachine2D < EvolutionMachine
+  def initialize(options = {})
+    @characters = options.delete(:characters) || (32..126).to_a.collect! { |c| c.chr }
+    super
+  end
+  
+  private
+  
+  def mutate(string)
+    string = string.clone.split("\n")
+    string.length.times do |line_number|
+      if rand(100) < 5
+        string[line_number].length.times do |i|
+          string[line_number][i] = random_char if rand(100) < 5
+        end
+      end
+    end
+    string.join("\n")
+  end
+
+  def random_char
+    @characters[rand(@characters.length)]
+  end
+
+  def select_fittest(children, target)
+    best_score = nil
+    fittest = nil
+    target = target.split("\n")
+    children.each do |child|
+      child = child.split("\n")
+      score = (0...child.length).inject(0) do |total_score, line_number|
+        line = child[line_number]
+        target_line = target[line_number]
+        total_score + (0...line.length).inject(0) do |line_score, i|
+          line[i] == target_line[i] ? line_score : line_score + 1
+        end
+      end
+      if(best_score.nil? || score < best_score)
+        fittest = child
+        best_score = score
+      end
+    end
+    fittest.join("\n")
+  end
+end
+
+start = <<-END
+###############
+###############
+###############
+###############
+###############
+###############
+###############
+END
+
+finish = <<-END
+   #########   
+ #           # 
+#    #   #    #
+#      #      #
+#   |_____|   #
+  #          # 
+    #######    
+END
+
+# If we allow all ascii characters
+# this will take way too long to run.
+characters = [' ', '#', '_', '|']
+
+machine = EvolutionMachine2D.new(:characters => characters)
+machine.evolve(start.chop, finish.chop)
